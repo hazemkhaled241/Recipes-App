0b9af40c3c4683dcfa76c31e3f0b425ea5405252
hazem khaled <hazemk778@gmail.com>
2023-06-25 20:35:53 +0300

    Enhancement in RecipeDetailsFragment.kt

--- app/src/main/java/com/example/recipesapp/presentation/home/recipe_details/RecipeDetailsFragment.kt
+++ app/src/main/java/com/example/recipesapp/presentation/home/recipe_details/RecipeDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -14,6 +15,7 @@ import com.example.recipesapp.R
 import com.example.recipesapp.databinding.FragmentRecipeDetailsBinding
 import com.example.recipesapp.domain.model.Meal
 import com.example.recipesapp.presentation.home.recipe_details.adapter.IngredientsAdapter
+import com.example.recipesapp.presentation.home.recipe_details.viewmodel.FavoriteState
 import com.example.recipesapp.presentation.home.recipe_details.viewmodel.RecipeDetailsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -44,9 +46,9 @@ class RecipeDetailsFragment : Fragment() {
         }
         recipeDetailsViewModel.getAllFavoriteRecipes()
         observe()
-        binding.rvIngredients.adapter=ingredientsAdapter
+        binding.rvIngredients.adapter = ingredientsAdapter
         setMealDate()
-        binding.ivBack.setOnClickListener{
+        binding.ivBack.setOnClickListener {
             findNavController().popBackStack()
         }
     }
@@ -75,35 +77,52 @@ class RecipeDetailsFragment : Fragment() {
         }
     }
 
-    private fun setMealDate(){
-        binding.ivRecipe.load(args.meal.image){
+    private fun setMealDate() {
+        binding.ivRecipe.load(args.meal.image) {
             placeholder(R.drawable.iv_plate)
         }
         ingredientsAdapter.updateList(args.meal.ingredients)
-        binding.tvName.text=args.meal.name
-        binding.tvRateValue.text=args.meal.rating
-        binding.tvRatingsValue.text=args.meal.ratings
-        binding.tvCountryValue.text=args.meal.country
-        binding.tvFatsValue.text=args.meal.fats
-        binding.tvCarbValue.text=args.meal.carbos
-        binding.tvCaloriesValue.text=args.meal.calories
-        binding.tvProteinsValue.text=args.meal.proteins
-        binding.tvFibersValue.text=args.meal.fibers
-        binding.tvRecipeDescription.text=args.meal.description
+        binding.tvName.text = args.meal.name
+        binding.tvRateValue.text = args.meal.rating
+        binding.tvRatingsValue.text = args.meal.ratings
+        binding.tvCountryValue.text = args.meal.country
+        binding.tvFatsValue.text = args.meal.fats
+        binding.tvCarbValue.text = args.meal.carbos
+        binding.tvCaloriesValue.text = args.meal.calories
+        binding.tvProteinsValue.text = args.meal.proteins
+        binding.tvFibersValue.text = args.meal.fibers
+        binding.tvRecipeDescription.text = args.meal.description
     }
 
     private fun handleIsFavorite() {
         isFavorite?.let { isFav ->
             isFavorite = !isFav
-            if (!isFav) {
-                binding.ivFavorite.setImageResource(R.drawable.baseline_favorite_24)
+            if (!isFav)
                 addToFavorite(args.meal)
-            } else {
-                binding.ivFavorite.setImageResource(R.drawable.baseline_favorite_border_24)
+            else
                 removeFromFavorite(args.meal)
+            observeOnFavoriteState()
+        }
+    }
+
+    private fun observeOnFavoriteState() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            recipeDetailsViewModel.favoriteResponse.collect() {
+                when (it) {
+                    is FavoriteState.AddedSuccessfully -> binding.ivFavorite.setImageResource(R.drawable.baseline_favorite_24)
+
+                    is FavoriteState.DeletedSuccessfully -> binding.ivFavorite.setImageResource(R.drawable.baseline_favorite_border_24)
+                    FavoriteState.Init -> Unit
+                    is FavoriteState.ShowError -> Toast.makeText(
+                        requireContext(),
+                        it.message,
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
             }
         }
     }
+
     private fun removeFromFavorite(meal: Meal) {
         recipeDetailsViewModel.deleteFromFavorite(meal)
     }
